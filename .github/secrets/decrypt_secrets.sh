5b506b0fbe1964bbd2d734daebedf38010989a4e
amrita154 <guptaamrita154@gmail.com>
2021-01-08 19:14:14 +0530

    changes

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -8,11 +8,11 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/b5bff837-9543-41fe-bd8a-de1255e11198.mobileprovision
 
 
-security create-keychain -p "" build.keychain
-security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security create-keychain -p "" ios-build.keychain
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/ios-build.keychain -P "" -A
 
-security list-keychains -s ~/Library/Keychains/build.keychain
-security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+security list-keychains -s ~/Library/Keychains/ios-build.keychain
+security default-keychain -s ~/Library/Keychains/ios-build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/ios-build.keychain
 
-security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/ios-build.keychain
