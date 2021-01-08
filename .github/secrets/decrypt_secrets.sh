de8873f9a8421a8d1435dc913598d05fc286c675
amrita154 <guptaamrita154@gmail.com>
2021-01-08 18:12:08 +0530

    changes

---
+++ .github/secrets/decrypt_secrets.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+gpg --quiet --batch --yes --decrypt --passphrase="sample" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="sample" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
+
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/b5bff837-9543-41fe-bd8a-de1255e11198.mobileprovision
+
+
+security create-keychain -p "" build.keychain
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
